e276e844fadaa5d1322ff11960e5f4e2198cb6f5
Arron Alsop <alsop.arron@gmail.com>
2023-11-29 16:23:26 -0800

    'wip'

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,11 +61,7 @@ router.post('/', (req, res) => {
     }
 })
 
-router.delete('/:id', (req, res) => {
-
-})
-
-router.put('/:id', async (req, res) => {
+router.delete('/:id', async  (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
         if (!maybe) {
@@ -83,8 +79,62 @@ router.put('/:id', async (req, res) => {
     }
 })
 
-router.get('/:id/messages', (req, res) => {
+router.put('/:id', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.findById(req.params.id)
+        .then(stuff => {
+            if (!stuff) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else {
+                return Post.update(req.params.id, req.body)
+            }
+        })
+        .then(data => {
+            if (data) {
+                return Post.findById(req.params.id)
+            }
+        })
+        .then(post => {
+            if (post) {
+                res.json(post)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ 
+                message: "The post with the specified ID does not exist",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+        
+    }
+})
 
+router.get('/:id/messages', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            const messages = await Post.findPostComments(req.params.id)
+            res.json(messages)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 
 
